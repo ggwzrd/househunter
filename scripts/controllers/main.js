cfcd9fb18b64510da0a0ce42e5df920f87ea2f48
giuliogallerini <giulio.gallerini.work@hotmail.com>
2016-09-30 15:06:22 +0200

    Clean code

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,21 +1,8 @@
 'use strict';
 
 angular.module("houseHunterApp")
-.controller('mainCtrl', function($scope){
-
-  // $scope.addTodo = function(){
-  //   var todo = {"value": "This is a new todo!"};
-  //   $scope.todos.unshift(todo);
-  // };
-  //
-  // dataService.getTodos(function(response){
-  //   $scope.todos = response.data;
-  // });
-  // $scope.deleteTodo = function(todo, index){
-  //   dataService.deleteTodo(todo);
-  //   $scope.todos.splice(index, 1);
-  // };
-  // $scope.saveTodo = function(todo, index){
-  //   dataService.saveTodo(todo);
-  // };
+.controller('mainCtrl', function($scope, groupsSrv){
+  groupsSrv.getGroups(function(response){
+    $scope.groups = response.data;
+  });
 });
